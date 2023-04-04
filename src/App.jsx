6c57f033496762105acf3f39ed24019e7de98d1f
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-04 13:05:43 +0300

    Added to  a href

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import './App.css'
 
 function App() {
@@ -6,6 +7,11 @@ function App() {
     <div className="App">
       <h1>hi</h1>
       <p>This is my diploma</p>
+
+      <nav>
+        <NavLink to>"/"Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
     </div>
   )
 }
