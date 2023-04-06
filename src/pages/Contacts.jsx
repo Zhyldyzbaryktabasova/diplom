13280ec11f6bb6d495b1092097c2b00894723340
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-06 20:10:15 +0300

    The contact details have been recorded

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,9 @@
+export default function Contacts() {
+  return(
+    <div className="Contacts">
+      <h1>Here is my contacts</h1>
+      <p>+996702938804</p>
+      <p>W/p/telegram </p>
+    </div>
+  )
+}
