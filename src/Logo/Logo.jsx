b7ddff19f635e16dde69f3695a1feba80a7ad899
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-06 20:14:28 +0300

    Added logo to component

---
+++ src/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import"./Logo.css";
+import logo from "../../assets/logo.svg";
+import { NavLink } from "react-router-dom";
+
+
+
+export default function Logo() {
+  return (
+    <div className="Logo">
+      <img src={h} alt="Website logo" />
+      </div>
+  );
+}
