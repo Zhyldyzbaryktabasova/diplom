ba6732382387ef4a2fa9959291b70ac639aef9c8
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-06 06:10:36 +0300

    Added route to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,14 @@ export default function App() {
       <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
       </nav>
 
       <main>
         <Routes>
           <Route path="/" element={<h2>Home</h2>} exact />
           <Route path="/about" element={<h2>About</h2>} />
+          <Route path="/contact" element={<h2>Contact</h2>} />
         </Routes>
       </main>
     </div>
