d21a696f4ccec4b07ca1454fe88ffecb0a1cc1e4
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-10 09:44:17 +0300

    We have written contact details

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,7 +2,7 @@ export default function Contacts() {
   return(
     <div className="Contacts">
       <h1>Here is my contacts</h1>
-      <p>+996702938804</p>
+      <p>+996702930488</p>
       <p>W/p/telegram </p>
     </div>
   )
