eb960093f1e8c1f6c587389e2d9b61672613fea9
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-13 11:47:15 +0300

    Move database fetch to App components

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
   return (
     <div className="Logo">
       <NavLink to="/">
-        <img src = {logo} alt="Website logo" />
+        <img src ="https://st3.depositphotos.com/15317184/19473/v/600/depositphotos_194733590-stock-illustration-woman-bag-sketchy-logo-bags.jpg" alt = "Website logo" />
       </NavLink>
     </div>
   );
