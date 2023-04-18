c4a3f06aecc61a7f5ed572b3cfbe2953c51badff
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-18 18:55:28 +0300

    Move database fetch to App component

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -6,19 +6,16 @@ import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
 
 export default function CategoryList() {
+  const { categories } = useContext(AppContext);
 
-  const {categories } = useContext(AppContext)
-  
   const output = categories.map((category) => (
     <li key={category.id}>
-      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
+      <NavLink to={"/categories/" + category.path}>{category.name}</NavLink>
     </li>
   ));
   return (
     <div className="CategoryList">
-      <ul>
-        {output}
-      </ul>
+      <ul>{output}</ul>
     </div>
   );
 }
