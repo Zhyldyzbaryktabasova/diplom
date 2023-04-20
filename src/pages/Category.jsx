5853da93324085cfd865d82d653597767ff2d0b1
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-20 06:55:00 +0300

    A picture was added to the main product, then it was deleted

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,24 +1,18 @@
-import {useContext } from "react";
+import { useContext } from "react";
 import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
+import ProductsList from "../components/ProductList/ProductList";
 
 export default function Category() {
-  const { params } = useMatch("/category/:path");
+  const { params } = useMatch("/categories/:path");
   const { categories } = useContext(AppContext);
-  const category = categories.find((category) =>params.path === category.path);
+  const category = categories.find((category) => params.path === category.path);
+
   return (
     <div className="Gategory">
-      <h1>{category.name}</h1>
-      <div className="ProductList">
-        <div className="Product">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_TOgpIg_YlY7CYKCMfifYq8SXCjGkCs-R-Rd13HxsmA&s" alt="Hare-gull"/>
-          <div>
-            <a href="/product/hare-gull">Hare-gull</a>
-            </div>
-          <p>500 som</p>
-          </div>
-         </div>
-      </div>
-    
+      <h1>{category ? category.name : "Loading..."}</h1>
+
+      <ProductsList category={category} />
+    </div>
   );
 }
