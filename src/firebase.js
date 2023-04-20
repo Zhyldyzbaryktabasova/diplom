8b3d7fe324643d621588e4d9ac44a510fac01264
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-20 12:12:01 +0300

    Add  auth provider

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { collection, getFirestore } from "firebase/firestore";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -11,13 +18,19 @@ const firebaseConfig = {
   projectId: "diploma-d9e2c",
   storageBucket: "diploma-d9e2c.appspot.com",
   messagingSenderId: "42802428302",
-  appId: "1:42802428302:web:a9c56b554a11c9d2400cdf"
+  appId: "1:42802428302:web:a9c56b554a11c9d2400cdf",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
 //получить список категорий (коллекция документов).
-export const categoryCollection = collection  (db, 'categories');
-export const productCollection =collection(db,'products');
+export const categoryCollection = collection(db, "categories");
+export const productCollection = collection(db, "products");
+
+const provider = new GoogleAuthProvider();
+export const logIn = () => signInWithPopup(auth, provider);
+export const logOut = () => signOut(auth);
+export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
