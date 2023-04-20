00215c8f91d4cad238ea70dfcc8c9fe5391a86bf
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-20 12:16:55 +0300

    added NotFound for file Category.jsx

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -2,11 +2,16 @@ import { useContext } from "react";
 import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 import ProductsList from "../components/ProductList/ProductList";
+import NotFound from "./NotFound";
 
 export default function Category() {
-  const { params } = useMatch("/categories/:path");
+  const { params } = useMatch("/category/:path");
   const { categories } = useContext(AppContext);
   const category = categories.find((category) => params.path === category.path);
+
+  if (!category) {
+    return <NotFound />;
+  }
 
   return (
     <div className="Gategory">
