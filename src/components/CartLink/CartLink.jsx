df549e1333e4efd6955556b58b9d5709c9310f9b
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-21 15:37:47 +0300

    Icon size is given

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -1,8 +1,9 @@
+import "./CartLink.css";
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 
-export default function CartLink() {
+export default function CartLink() { 
   const { cart } = useContext(AppContext);
 
   const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
@@ -10,9 +11,14 @@ export default function CartLink() {
   return (
     <div className="CartLink">
       <Link to="/cart">
-        Cart ({total})
-      </Link>
-    </div>
+        <a href ="/cart">
+        <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt ="Cart-pht"/>
+        "("
+        "0"
+        ")"
+          </a>
+          </Link>
+      </div>
   );
 }
-
+  
