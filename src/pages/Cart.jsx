784363209a905bec8981ca58bc13a0299516db7f
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-21 08:27:22 +0300

    QTY change

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,15 @@
 import { useContext } from "react";
 import "./Cart.css";
 import CartList from "../components/CartList/CartList";
+import OrderForm from "../components/OrderForm/OrderFotm";
+
 
 export default function Cart() {
   return (
     <div className="Cart"> 
-    <img src="https://w7.pngwing.com/pngs/188/963/png-transparent-computer-icons-shopping-cart-cheat-angle-service-desktop-wallpaper.png" alt = "ikon" />
+    
       <CartList/>
+      <OrderForm />
     </div>
   );
 }
