4b7a81ee5512579b58b8d2d6b741b4925854358b
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-24 16:28:20 +0300

    The text in about has changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
   return(
     <div className="About">
       <h1>Our company founded 1980</h1>
-      <p>We have many bags</p>
+      <p>You will receive your order the next day in Bishkek and other 60 major cities of Russia! Our products are always of high quality and convenient for people. People's interest in our products is very strong</p>
     </div>
   )
 }
