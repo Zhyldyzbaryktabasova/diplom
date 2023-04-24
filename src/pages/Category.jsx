8a17a73e952acfcdc1a58ea4e60f9553327180cd
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-24 15:27:11 +0300

    delete about jsx

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -3,6 +3,7 @@ import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 import ProductsList from "../components/ProductList/ProductList";
 import NotFound from "./NotFound";
+import "./Category.css";
 
 export default function Category() {
   const { params } = useMatch("/category/:path");
