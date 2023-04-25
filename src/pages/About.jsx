56b2380606afd62c962135dc7441edb8cc43eaf7
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-25 12:36:53 +0300

    bug got better

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ export default function About() {
 
   return(
     <div className="About">
-      <h1>Our company founded 1980</h1>
+      <h1>Our company founded 1980  2132132513521</h1>
       <p>You will receive your order the next day in Bishkek and other 60 major cities of Russia! Our products are always of high quality and convenient for people. People's interest in our products is very strong</p>
     </div>
   )
