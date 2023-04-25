1c5d306d6c55e32e0f8ef0376f94acab5ce9fb85
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-25 12:35:40 +0300

    bug got better

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
 
-export default function CartLink() { 
+export default function CartLink() {
   const { cart } = useContext(AppContext);
 
   const total = Object.values(cart).reduce((acc, num) => acc + num, 0);
@@ -11,14 +11,14 @@ export default function CartLink() {
   return (
     <div className="CartLink">
       <Link to="/cart">
-        <a href ="/cart">
-        <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt ="Cart-pht"/>
-        "("
-        "0"
-        ")"
-          </a>
-          </Link>
-      </div>
+        <a href="/cart">
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
+            alt="Cart-pht"
+          />
+          {total}
+        </a>
+      </Link>
+    </div>
   );
 }
-  
