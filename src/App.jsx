91ed5d30a60e95e0562bd0ebaa7941e28c1528f0
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-27 06:04:09 +0300

    Move database fetch to App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,48 +4,50 @@ import Home from "./pages/Home";
 import Category from "./pages/Category";
 import { createContext, useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore";
-import { categoryCollection, onAuthChange, productCollection } from "./firebase";
+import {
+  categoryCollection,
+  onAuthChange,
+  orderCollection,
+  productCollection,
+} from "./firebase";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import Product from "./pages/Product";
 import ThankYou from "./pages/ThankYou";
+import Orders from "./pages/Orders";
 
 export const AppContext = createContext({
   categories: [],
   products: [],
+  orders: [],
 
   // корзина
   cart: {},
   setCart: () => {},
-
   user: null, // здесь будет храниться информация про пользователя
 });
-
 export default function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [orders, setOrders] = useState([]);
 
   // состояние которое хранит информацию пользователя
   const [user, setUser] = useState(null);
-
   // корзина
   const [cart, setCart] = useState(() => {
     // восстановить содержимое корзинки из памяти браузера.
     return JSON.parse(localStorage.getItem("cart")) || {};
   });
-
   // выполнить эту функцию только когда содержимое корзинки меняется
   useEffect(() => {
     // сохранить содержимое корзинки в памяти браузера
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
-
   // выполнить эту функцию только один раз
   useEffect(() => {
     // получить категории из списка категорий
     getDocs(categoryCollection).then((snapshot) => {
       // категории будут храниться в snapshot.docs
-
       // создать массив для категорий
       const newCategories = [];
       // заполнить массив данными из списка категорий
@@ -53,17 +55,14 @@ export default function App() {
         // doc = категория
         const category = doc.data();
         category.id = doc.id;
-
         newCategories.push(category);
       });
       // задать новый массив как состояние комапо
       setCategories(newCategories);
     });
-
     // получить продукты из списка продуктов
     getDocs(productCollection).then((snapshot) => {
       // продукты будут храниться в snapshot.docs
-
       // создать массив для продуктов
       const newProducts = [];
       // заполнить массив данными из списка продвук
@@ -71,21 +70,40 @@ export default function App() {
         // doc = продукт
         const product = doc.data();
         product.id = doc.id;
-
         newProducts.push(product);
       });
       // задать новый массив как состояние комапо
       setProducts(newProducts);
     });
 
-    onAuthChange(user => {
+    // получить продукты из списка продуктов
+    getDocs(orderCollection).then((snapshot) => {
+      // продукты будут храниться в snapshot.docs
+
+      // создать массив для продуктов
+      const newOrders = [];
+      // заполнить массив данными из списка продвук
+      snapshot.docs.forEach((doc) => {
+        // doc = продукт
+        const order = doc.data();
+        order.id = doc.id;
+
+        newOrders.push(order);
+      });
+      // задать новый массив как состояние комапо
+      setOrders(newOrders);
+    });
+
+    onAuthChange((user) => {
       setUser(user);
     });
   }, []);
 
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories, products, cart, setCart, user }}>
+      <AppContext.Provider
+        value={{ categories, products, cart, setCart, user, orders }}
+      >
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -93,7 +111,8 @@ export default function App() {
             <Route path="/category/:path" element={<Category />} />
             <Route path="/product/:path" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/thank you" element= {<ThankYou />} />
+            <Route path="/thank-you" element={<ThankYou />} />
+            <Route path="/orders" element={<Orders />} />
 
             <Route path="*" element={<NotFound />} />
           </Routes>
