9dfc4b6cdd1579b4d1b4ee76127eb0658408c5ac
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-04-27 06:09:29 +0300

    Add cart link compnent

--- src/components/CartLink/CartLink.jsx
+++ src/components/CartLink/CartLink.jsx
@@ -1,7 +1,7 @@
-import "./CartLink.css";
 import { useContext } from "react";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
+import "./CartLink.css";
 
 export default function CartLink() {
   const { cart } = useContext(AppContext);
@@ -12,12 +12,11 @@ export default function CartLink() {
     <div className="CartLink">
       <Link to="/cart">
         <a href="/cart">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
-            alt="Cart-pht"
-          />
-          {total}
-        </a>
+          <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png"
+            alt="Cart-pht"/>
+            {total}
+            </a>
+        
       </Link>
     </div>
   );
