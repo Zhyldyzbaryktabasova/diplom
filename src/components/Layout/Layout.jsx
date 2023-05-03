a6276fd8b167ec6137f0666262277b6b6745d1de
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-05-03 18:44:42 +0300

    added Drawer component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,28 +1,36 @@
+import { useState } from "react";
 import Auth from "../Auth/Auth";
 import CartLink from "../CartLink/CartLink";
-import GategoryList from "../CategoryList/CategoryList";
+import CategoryList from "../CategoryList/CategoryList";
+import Drawer from "../Drawer/Drawer";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
+import NavToggle from "../NavToggle/NavToggle";
 import "./Layout.css";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className="Layout">
       <header>
-        <Logo/>
+        <Logo />
         <Nav />
-        <CartLink/>
-        <Auth />
-        </header>
+        <NavToggle callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
+        <div>
+          <CartLink />
+          <Auth />
+        </div>
+      </header>
       <aside>
-        <GategoryList/>
+        <CategoryList />
       </aside>
-      <main>
-        {props.children}
-        </main>
-      <footer>
-        FOOTER
-      </footer>
-     </div>
+      <main>{props.children}</main>
+      <footer>FOOTER</footer>
+    </div>
   );
 }
