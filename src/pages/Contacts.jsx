79ac5d8bc0389779e70630091e6eac568f5ade04
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-05-10 18:50:37 +0300

    added p with strong

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,23 @@
+import eyes from "../assets/eyes.png"
+import "./Contacts.css"
+
+
 export default function Contacts() {
-  return(
+  return (
     <div className="Contacts">
-      <h1>Here is my contacts</h1>
-      <p>+996702930488</p>
-      <p>W/p/telegram</p>
+      <h1>If you want to contact us, call the following phone number</h1>
+      <div className= "Row">
+        <div className="Col-6">
+          Our contacts: <br />
+         +996702930488 <br/>
+         +996702880488 <br />
+         +996703978965 <br />
+    </div>
+    <div className="Col-6">
+      <img className="Eyes" src={eyes} alt="photo-eyes" />
+    </div>
+    </div>
+    <p><strong> Our adress:</strong>121 Lenina St, 8th house</p>
     </div>
   )
 }
