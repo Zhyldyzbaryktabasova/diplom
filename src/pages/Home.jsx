72d7fbe880e653b2c79b94a7d73657aef79d7d8e
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-05-12 08:41:31 +0300

    The error has been corrected

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,13 @@ export default function Home() {
       <h1>Bags shop</h1>
       <p>Quality bags</p>
       <p>You will find any bag in our store</p>
+      <img src="https://avatars.mds.yandex.net/get-altay/4552098/2a0000017a16dd914f26922e5e4e39404bc8/XXL_height"/>
+     </div>
 
-    </div>
+
   )
 }
+
+
+
+   
