b84fb77b79e9f37081c196970989276899ba6c0f
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-05-17 11:50:57 +0300

    video added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,23 @@ export default function Home() {
   return (
     <div className="Home">
       <h1>Bags shop</h1>
-      <p>Quality bags</p>
-      <p>You will find any bag in our store</p>
-      <img src="https://avatars.mds.yandex.net/get-altay/4552098/2a0000017a16dd914f26922e5e4e39404bc8/XXL_height"/>
+      <p>Quality bags</p> 
+      <p>You will find any bag in our store.The catalog of our online store includes: business leather goods for
+            men and women, leather and textile models, bags for every season,
+            color and classic options, and most importantly - fashionable and
+            really the best! More than a wide range will forever decide for you
+            the question of where to buy a bag, and the excellent quality of our
+            goods will delight every day. We follow the news and hurry to offer
+            them to you at affordable prices as soon as they appear in the
+            catalogs of our partners. If you are looking for a fashionable bag,
+            you will definitely like it here!</p>
+      <video controls autoPlay="true" loop="true">
+        <source
+        src="https://st4.depositphotos.com/34691230/39382/v/600/depositphotos_393824718-stock-video-stylish-girl-with-glasses-and.mp4"
+        tupe='video/mp4;codecs="avc1.42E01E,mp4.40.2"'
+        />
+        </video>
+      
      </div>
 
 
