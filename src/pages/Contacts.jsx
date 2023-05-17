2af18bcce77a15a79c524a60e402ee1ffc7c96d9
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-05-17 11:53:07 +0300

    added contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,23 +1,24 @@
-import eyes from "../assets/eyes.png"
 import "./Contacts.css"
 
 
 export default function Contacts() {
   return (
     <div className="Contacts">
-      <h1>If you want to contact us, call the following phone number</h1>
-      <div className= "Row">
+      <div className="Row">
         <div className="Col-6">
-          Our contacts: <br />
-         +996702930488 <br/>
-         +996702880488 <br />
-         +996703978965 <br />
+    <strong>Contact us</strong>
+    <p>
+      {" "}
+      +996 702 938 804</p>
+    <p>+996 702 880 493</p>
+    <p>+996 702 049 388</p>
     </div>
     <div className="Col-6">
-      <img className="Eyes" src={eyes} alt="photo-eyes" />
+      <div className="Contacts-r">
+        <p>zhyldyz.baryktabasova.ch@gmail.com</p>
+      </div>
     </div>
     </div>
-    <p><strong> Our adress:</strong>121 Lenina St, 8th house</p>
     </div>
   )
 }
